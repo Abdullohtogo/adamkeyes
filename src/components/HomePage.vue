6e4155bdcb1a2a9386e8dffa99df179082491f19
Abdullohyuldoshev <ayuldoshev08@gmail.com>
2023-10-12 11:45:20 +0500

    first commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,225 @@
+<template>
+    <main class="bg-black">
+        <div class="xl:ml-36 lg:ml-16 md:ml-10 sm:ml-5 xs:ml-2">
+            <br>
+            <nav class="flex xl:gap-[700px] xl:flex-row lg:gap-[400px] lg:flex-row md:gap-[200px] md:flex-row sm:flex-row sm:gap-[50px] xs:flex-col">
+                <h1 class="text-white text-3xl font-semibold xs:text-center">adamkeyes</h1>
+                <div class="mt-2">
+                    <img src="../assets/img/Group%2012.svg" alt="" class="xs:ml-[85px]">
+                </div>
+            </nav>
+            <img src="../assets/img/Group%2026.png" alt="" class="translate-y-20">
+            <section class="flex mt-20">
+                <div class="">
+                    <h1 class="text-white font-bold xl:text-[88px] xl:w-[750px]">Nice to meet you! I’m Adam Keyes.</h1><div class="bg-green xl:w-[515px] xl:h-[6px] xl:translate-x-[140px] xl:-translate-y-3"></div>
+                    <p class="xl:text-[18px] gray xl:w-[445px] xl:mt-[20px]">Based in the UK, I’m a front-end developer passionate about building accessible web apps that users love.</p>
+                    <a href="#contact" class="flex flex-col text-white xl:mt-[40px] text-[16px]">CONTACT ME</a>
+                    <div class="bg-green w-[95px] h-[2px] mt-2 hover:text-green-400"></div>
+                </div>
+                <img src="../assets/img/image-removebg-preview(4).png" alt="" class="xl:w-[444px] xl:h-[618px] xl:-translate-y-20 xl:-translate-x-11">
+            </section>
+            <div class="border-solid border-gray-300 border-[1px] w-[87%]"></div>
+            <section>
+                <div class="w-[87%] h-[00px] gap-[200px] flex mt-24 mb-40">
+                    <div class="w-[300px]">
+                        <h1 class="text-white text-[48px]">HTML</h1>
+                        <p class="text-white mt-1">4 Years Experience</p>
+                    </div>
+                    <div class="">
+                        <h1 class="text-white text-[48px]">CSS</h1>
+                        <p class="text-white mt-1">4 Years Experience</p>
+                    </div>
+                    <div class="">
+                        <h1 class="text-white text-[48px]">JavaScript</h1>
+                        <p class="text-white mt-1">4 Years Experience</p>
+                    </div>
+                </div>
+                <div  class="w-[87%] gap-[200px] flex mt-24 mb-20">
+                    <div class="">
+                        <h1 class="text-white text-[48px]">Accessibility</h1>
+                        <p class="text-white mt-1">4 Years Experience</p>
+                    </div>
+                    <div class="">
+                        <h1 class="text-white text-[48px]">React</h1>
+                        <p class="text-white mt-1">3 Years Experience</p>
+                    </div>
+                    <div class="">
+                        <h1 class="text-white text-[48px]">Sass</h1>
+                        <p class="text-white mt-1">3 Years Experience</p>
+                    </div>
+                </div>
+                <br>
+            </section>
+            <section>
+                <div class="flex gap-[700px]">
+                    <h1 class="text-white text-[88px]">Projects</h1>
+                    <div class="mt-3">
+                        <a href="#contact" class="flex flex-col text-white xl:mt-[40px] text-[16px]">CONTACT ME</a>
+                        <div class="bg-green w-[95px] h-[2px] mt-2 hover:text-green-400"></div>
+                    </div>
+                </div>
+                <br>
+                <div class="flex gap-[25px]">
+                    <div>
+                        <div>
+                            <div class="bitmap bitmap1">
+                                <br>
+                                <div class="mt-[150px] ml-[220px] none">
+                                    <div class="mt-3">
+                                        <a href="#" class="flex flex-col text-white xl:mt-[40px] text-[16px]">VIEW PROJECT</a>
+                                        <div class="bg-green w-[108px] h-[2px] mt-2 hover:text-green-400"></div>
+                                    </div>
+                                    <div class="mt-3">
+                                        <a href="#" class="flex flex-col text-white xl:mt-[40px] text-[16px]">VIEW CODE</a>
+                                        <div class="bg-green w-[80px] h-[2px] mt-2 hover:text-green-400"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="mt-10">
+                                <h1 class="text-white text-[24px]">DESIGN PORTFOLIO</h1>
+                                <p class="text-[18px] text-[#D9D9D9]">HTML CSS</p>
+                                <br>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="bitmap bitmap2">
+                                <br>
+                                <div class="mt-[150px] ml-[220px] none">
+                                    <div class="mt-3">
+                                        <a href="#" class="flex flex-col text-white xl:mt-[40px] text-[16px]">VIEW PROJECT</a>
+                                        <div class="bg-green w-[108px] h-[2px] mt-2 hover:text-green-400"></div>
+                                    </div>
+                                    <div class="mt-3">
+                                        <a href="#" class="flex flex-col text-white xl:mt-[40px] text-[16px]">VIEW CODE</a>
+                                        <div class="bg-green w-[80px] h-[2px] mt-2 hover:text-green-400"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="mt-10">
+                                <h1 class="text-white text-[24px]">E-LEARNING LANDING PAGE</h1>
+                                <p class="text-[18px] text-[#D9D9D9]">HTML CSS</p>
+                                <br>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="bitmap bitmap3">
+                                <br>
+                                <div class="mt-[150px] ml-[220px] none">
+                                    <div class="mt-3">
+                                        <a href="#" class="flex flex-col text-white xl:mt-[40px] text-[16px]">VIEW PROJECT</a>
+                                        <div class="bg-green w-[108px] h-[2px] mt-2 hover:text-green-400"></div>
+                                    </div>
+                                    <div class="mt-3">
+                                        <a href="#" class="flex flex-col text-white xl:mt-[40px] text-[16px]">VIEW CODE</a>
+                                        <div class="bg-green w-[80px] h-[2px] mt-2 hover:text-green-400"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mt-10">
+                            <h1 class="text-white text-[24px]">TODO WEB APP</h1>
+                            <p class="text-[18px] text-[#D9D9D9]">HTML CSS JAVASCRIPT</p>
+                            <br>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <div class="bitmap bitmap4">
+                                <br>
+                                <div class="mt-[150px] ml-[220px] none">
+                                    <div class="mt-3">
+                                        <a href="#" class="flex flex-col text-white xl:mt-[40px] text-[16px]">VIEW PROJECT</a>
+                                        <div class="bg-green w-[108px] h-[2px] mt-2 hover:text-green-400"></div>
+                                    </div>
+                                    <div class="mt-3">
+                                        <a href="#" class="flex flex-col text-white xl:mt-[40px] text-[16px]">VIEW CODE</a>
+                                        <div class="bg-green w-[80px] h-[2px] mt-2 hover:text-green-400"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="mt-10">
+                                <h1 class="text-white text-[24px]">ENTERTAINMENT WEB APP</h1>
+                                <p class="text-[18px] text-[#D9D9D9]">HTML CSS JAVASCRIPT</p>
+                                <br>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="bitmap bitmap5">
+                                <br>
+                                <div class="mt-[150px] ml-[220px] none">
+                                    <div class="mt-3">
+                                        <a href="#" class="flex flex-col text-white xl:mt-[40px] text-[16px]">VIEW PROJECT</a>
+                                        <div class="bg-green w-[108px] h-[2px] mt-2 hover:text-green-400"></div>
+                                    </div>
+                                    <div class="mt-3">
+                                        <a href="#" class="flex flex-col text-white xl:mt-[40px] text-[16px]">VIEW CODE</a>
+                                        <div class="bg-green w-[80px] h-[2px] mt-2 hover:text-green-400"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="mt-10">
+                                <h1 class="text-white text-[24px]">MEMORY GAME</h1>
+                                <p class="text-[18px] text-[#D9D9D9]">HTML CSS JAVASCRIPT</p>
+                                <br>
+                            </div>
+                        </div>
+                        <div>
+                            <div class="bitmap bitmap6">
+                                <br>
+                                <div class="mt-[150px] ml-[220px] none">
+                                    <div class="mt-3">
+                                        <a href="#" class="flex flex-col text-white xl:mt-[40px] text-[16px]">VIEW PROJECT</a>
+                                        <div class="bg-green w-[108px] h-[2px] mt-2 hover:text-green-400"></div>
+                                    </div>
+                                    <div class="mt-3">
+                                        <a href="#" class="flex flex-col text-white xl:mt-[40px] text-[16px]">VIEW CODE</a>
+                                        <div class="bg-green w-[80px] h-[2px] mt-2 hover:text-green-400"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="mt-10">
+                                <h1 class="text-white text-[24px]">ART GALLERY SHOWCASE</h1>
+                                <p class="text-[18px] text-[#D9D9D9]">HTML CSS JAVASCRIPT</p>
+                                <br>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <br>
+            <br>
+        </div>
+        <section class="bg-[#242424]">
+            <div class="container xl:ml-36" id="contact">
+                <br>
+                <div class="flex">
+                    <div>
+                        <h1 class="text-white text-[88px]">Contact</h1>
+                        <p class="text-[18px] w-[445px] text-white">I would love to hear about your project and how I could help. Please fill in the form, and I’ll get back to you as soon as possible.</p>
+                    </div>
+                    <form class="ml-52 mt-6 flex flex-col">
+                        <input type="text" placeholder="Name" class="input mb-5">
+                        <br>
+                        <input type="email" placeholder="Email" class="input mb-5">
+                        <br>
+                        <textarea type="text" placeholder="Message" class="input w-[450px] mb-5"></textarea>
+                        <div class="ml-[350px]">
+                            <button type="submit" class="border-none flex flex-col text-white xl:mt-[40px] text-[16px]">CONTACT ME</button>
+                            <div class="bg-green w-[95px] h-[2px] mt-2 hover:text-green-400"></div>
+                        </div>
+                    </form>
+                </div>
+                <div class="border-solid border-gray-300 border-[1px] w-[104%] mt-10 mb-16"></div>
+                <footer class="flex xl:gap-[700px] xl:flex-row lg:gap-[400px] lg:flex-row md:gap-[200px] md:flex-row sm:flex-row sm:gap-[50px] xs:flex-col">
+                    <h1 class="text-white text-3xl font-semibold xs:text-center">adamkeyes</h1>
+                    <div class="mt-2">
+                        <img src="../assets/img/Group%2012.svg" alt="" class="xs:ml-[85px]">
+                    </div>
+                </footer>
+                <br>
+            </div>
+        </section>
+    </main>
+</template>
+<script>
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+    screens:{
+      xs:'375px',
+      sm:'540px',
+      md:'710px',
+      lg:'940px',
+      xl:'1100px'
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
